ad0eab8590f18dea0b3616842c86ff7f9429205a
AlbertoN03 <skybluex32@gmail.com>
2019-11-13 22:53:36 +0000

    11/13

--- js/script.js
+++ js/script.js
@@ -6,8 +6,16 @@
 /* global $ */
  $("#shoot").click(function(){
  $("#userChoice").text($("#input").val());
-}
-
-
+})
+var sign = Math.random();
+var choice=[Rock,Scissor,Paper];
+/*if(sign > .6){
+ ("Rock");
+}else if(sign > .3){
+("Paper");
+}else (sign > 0)
+("Scissor");
+;
+/*
 // DOCUMENT READY FUNCTION BELOW
 
